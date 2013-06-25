943c1c2e3bd441957c8368404dc3ff365232172e
sxyx2008 <sxyx2008@163.com>
2013-06-25 13:47:58 +0800

    add all files.

---
+++ models/comment.js
@@ -0,0 +1,79 @@
+/**
+ * 评论模块
+ * @type {*}
+ */
+
+var MongoClient = require('mongodb').MongoClient;
+
+function Comment(name, day, title, comment) {
+    this.name = name;
+    this.day = day;
+    this.title = title;
+    this.comment = comment;
+}
+
+module.exports = Comment;
+
+
+Comment.prototype.save = function(callback) {
+    var name = this.name,
+        day = this.day,
+        title = this.title,
+        comment = this.comment;
+    MongoClient.connect('mongodb://root:root@localhost:27017/blog',{},function(err, db){
+        if (err) {
+            return callback(err);
+        }else{
+            db.collection('posts', function (err, collection) {
+                if (err) {
+                    db.close();
+                    return callback(err);
+                }
+                //通过用户名、发表日期及标题查找文档，并把一条留言添加到该文档的 comments 数组里
+                collection.findAndModify({"name":name,"time.day":day,"title":title}
+                    , [['time',-1]]
+                    , {$push:{"comments":comment}}
+                    , {new: true}
+                    , function (err,comment) {
+                        db.close();
+                        callback(null);
+                    });
+            });
+        }
+    });
+
+};
+
+
+
+
+/*
+var mongodb = require('./db');
+Comment.prototype.save = function(callback) {
+    var name = this.name,
+        day = this.day,
+        title = this.title,
+        comment = this.comment;
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        db.authenticate('root','root',{},function(err,result){
+            db.collection('posts', function (err, collection) {
+                if (err) {
+                    mongodb.close();
+                    return callback(err);
+                }
+                //通过用户名、发表日期及标题查找文档，并把一条留言添加到该文档的 comments 数组里
+                collection.findAndModify({"name":name,"time.day":day,"title":title}
+                    , [['time',-1]]
+                    , {$push:{"comments":comment}}
+                    , {new: true}
+                    , function (err,comment) {
+                        mongodb.close();
+                        callback(null);
+                    });
+            });
+        });
+    });
+};*/
